d0774d3996a96c4bc6376b518d93860bf642b25f
Hamashi <hamashii@gmail.com>
2017-01-18 16:38:30 +0100

    Eclipse project created, movie object made

---
+++ BretonMarshallProject/src/Movie.java
@@ -0,0 +1,36 @@
+
+public class Movie {
+
+	private String title;
+	private String mainActor;
+	private String director;
+	private String year;
+	
+	public Movie(String title, String mainActor, String director, String year)
+	{
+		this.title = title;
+		this.mainActor = mainActor;
+		this.director = director;
+		this.year = year;
+	}
+	
+	public String getTitle()
+	{
+		return this.title;
+	}
+	
+	public String getMainActor()
+	{
+		return this.mainActor;
+	}
+	
+	public String getDirector()
+	{
+		return this.director;
+	}
+	
+	public String getYear()
+	{
+		return this.year;
+	}
+}
