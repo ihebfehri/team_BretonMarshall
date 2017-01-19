245313e68ea35a773f27ad772fbf8f32e7d14790
Iheb <ihebfehri17@gmail.com>
2017-01-19 09:46:25 +0100

    fixed problem i had with my ide

--- BretonMarshallProject/src/arrayListCollection.java
+++ BretonMarshallProject/src/arrayListCollection.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class arrayListColl {
+public class arrayListCollection {
 
     public static void main(String[] args) { 
        /*Creation of ArrayList: I'm going to add String
@@ -11,26 +11,26 @@ public class arrayListColl {
 
 
 	  /*This is how elements should be added to the array list*/
-	  obj.add("Ajeet");
+	  arrayListColl.add("Ajeet");
 
 
 	  /* Displaying array list elements */
-	  System.out.println("Currently the array list has following elements:"+obj);
+	  System.out.println("Currently the array list has following elements:"+arrayListColl);
 
 	  /*Add element at the given index*/
-	  obj.add(0, "Rahul");
-	  obj.add(1, "Justin");
+	  arrayListColl.add(0, "Rahul");
+	  arrayListColl.add(1, "Justin");
 
 	  /*Remove elements from array list like this*/
-	  obj.remove("Chaitanya");
-	  obj.remove("Harry");
+	  arrayListColl.remove("Chaitanya");
+	  arrayListColl.remove("Harry");
 
-	  System.out.println("Current array list is:"+obj);
+	  System.out.println("Current array list is:"+arrayListColl);
 
 	  /*Remove element from the given index*/
-	  obj.remove(1);
+	  arrayListColl.remove(1);
 
-	  System.out.println("Current array list is:"+obj);
+	  System.out.println("Current array list is:"+arrayListColl);
 
 
     }
